24430952c4e985055282eb339ddb8d19d562fb82
Deny Febriawan <denyfebriawan@gmail.com>
2022-11-24 21:35:58 +0700

    change db link

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/spa_mall")
+    .connect('mongodb://denyfebriawan:denyfebriawan@ac-eqp3ojr-shard-00-00.bp160w6.mongodb.net:27017,ac-eqp3ojr-shard-00-01.bp160w6.mongodb.net:27017,ac-eqp3ojr-shard-00-02.bp160w6.mongodb.net:27017/?ssl=true&replicaSet=atlas-jrwx8m-shard-0&authSource=admin&retryWrites=true&w=majority')
     .catch(err => console.log(err));
 };
 
